8dd2ecc16d8a6557b65ece6ab690fee794475003
luzi2333 <106014159+luzi2333@users.noreply.github.com>
2022-07-10 19:20:18 +0800

    Add files via upload

---
+++ 置换代码.c
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define N 5
+
+
+
+void main()
+{
+	int a[N] = { 1,2,3,4,5 }, temp, i;
+	for (i = 0; i <N / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[N - i - 1];
+		a[N - i - 1] = temp;		
+
+	}
+	for (i = 0; i < N ; i++)
+	{
+		printf("%d", a[i]);
+	}
+}
